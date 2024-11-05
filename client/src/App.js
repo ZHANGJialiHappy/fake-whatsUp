82b8069481dcee4bf06e8a1f52f8ab4b2f4794a4
Jiali Zhang <zhangjiali86@gmail.com>
2024-11-05 23:45:52 +0100

    router + parseService.js + Mainlayout

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,13 @@
-import './App.css';
-// Import Parse minified version
-import Parse from 'parse';
-
-// Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = 'BPo17lq5JC17DCclEGqLD4h0rnqs2rqgzF29mcTl';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'Jcx8pcnXdv3w7wdFHooiAzlW56JGr2kEQOqBJrRU';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+import MainLayout from './components/layout/MainLayout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/chat/:selfChatId" element={<MainLayout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
