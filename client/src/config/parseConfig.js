a156b9f643570ba9af265d2138dc0774a9ba206e
Jiali Zhang <zhangjiali86@gmail.com>
2024-11-19 22:50:42 +0100

    delete .env

--- client/src/config/parseConfig.js
+++ client/src/config/parseConfig.js
@@ -1,8 +1,8 @@
 import Parse from "parse";
 
-const PARSE_APPLICATION_ID = process.env.REACT_APP_PARSE_APPLICATION_ID;
+const PARSE_APPLICATION_ID = "BPo17lq5JC17DCclEGqLD4h0rnqs2rqgzF29mcTl";
 const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-const PARSE_JAVASCRIPT_KEY = process.env.REACT_APP_PARSE_JAVASCRIPT_KEY;
+const PARSE_JAVASCRIPT_KEY = "Jcx8pcnXdv3w7wdFHooiAzlW56JGr2kEQOqBJrRU";
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 
